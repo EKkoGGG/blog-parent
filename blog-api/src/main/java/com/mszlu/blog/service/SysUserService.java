0af6bdb169d34319881b28974fe22fabef611aaf
曾伟杰 <330564052@qq.com>
2021-07-31 09:39:18 +0800

    更新忽略2

--- blog-api/src/main/java/com/mszlu/blog/service/SysUserService.java
+++
@@ -1,7 +0,0 @@
-package com.mszlu.blog.service;
-
-import com.mszlu.blog.dao.pojo.SysUser;
-
-public interface SysUserService {
-    SysUser findUserById(Long id);
-}
--- blog-api/src/main/java/com/mszlu/blog/service/TagService.java
+++
@@ -1,9 +0,0 @@
-package com.mszlu.blog.service;
-
-import com.mszlu.blog.vo.TagVo;
-
-import java.util.List;
-
-public interface TagService {
-    List<TagVo> findTagsByArticleId(Long articleId);
-}
