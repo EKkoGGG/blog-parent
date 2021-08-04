369f167a50b99a1ab28039cf9b44f135bc7e7cb0
ZWJ <330564052@qq.com>
2021-08-05 00:08:47 +0800

    添加后台

---
+++ blog-admin/src/main/java/com/mszlu/blog/admin/mapper/AdminMapper.java
@@ -0,0 +1,7 @@
+package com.mszlu.blog.admin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mszlu.blog.admin.pojo.Admin;
+
+public interface AdminMapper extends BaseMapper<Admin> {
+}
